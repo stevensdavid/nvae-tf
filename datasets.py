ba37bc2710b65aad5e891f9b392ad6856d9f453a
Amy Chen <ajchen@kth.se>
2020-10-07 13:23:11 +0200

    add load_mnist

--- datasets.py
+++ datasets.py
@@ -1,1 +1,13 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
+
+
+def load_mnist(batch_size):
+    train_ds, test_ds = tfds.load('mnist', split=['train', 'test'], batch_size=batch_size, as_supervised=True)
+    return train_ds, test_ds
+
+def load_celeba():
+    # tfds.load('celeb_a')
+
+if __name__ == "__main__":
+    load_celeba()
