3012781ea214b961d55d599efd6ad711b3d21638
David Stevens <dstevens@kth.se>
2020-10-15 22:05:28 +0200

    Apply same MNIST transforms as NVLabs

--- datasets.py
+++ datasets.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
-
+import tensorflow_probability as tfp
 
 def load_mnist(batch_size):
     train_ds, test_ds = tfds.load('mnist', split=['train', 'test'], batch_size=batch_size, as_supervised=True)
-    def normalize(image, label):
-        return tf.cast(image, tf.float32) / 255, label
+    def transform(image, label):
+        image = tf.image.resize_with_crop_or_pad(image, 32, 32)
+        image = tf.cast(image, dtype=tf.float32)
+        image = tfp.distributions.Bernoulli(probs=image).sample()
+        return image, label
 
-    return train_ds.map(normalize), test_ds.map(normalize)
+    return train_ds.map(transform), test_ds.map(transform)
 
 def load_celeba():
     # tfds.load('celeb_a')
