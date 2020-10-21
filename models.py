2ace05ea2d452b3ba65335ee95ac14a62953b869
torang <torang@kth.se>
2020-10-21 13:13:58 +0200

    Untested draft

--- models.py
+++ models.py
@@ -29,6 +29,7 @@ class NVAE(tf.keras.Model):
         sr_lambda,
         scale_factor,
         total_epochs,
+        n_total_iterations,
         **kwargs
     ):
         super().__init__(**kwargs)
@@ -70,6 +71,8 @@ class NVAE(tf.keras.Model):
         # Updated at start of each epoch
         self.epoch = 0
         self.total_epochs = total_epochs
+        # Updated for each gradient pass, training step
+        self.steps = 0
 
     def call(self, inputs):
         x = self.preprocess(inputs)
@@ -102,12 +105,14 @@ class NVAE(tf.keras.Model):
             spectral_loss, bn_loss = self.calculate_spectral_and_bn_loss()
             # warming up KL term for first 30% of training
             beta = min(self.epoch / 0.3 * self.total_epochs, 1)
-            kl_loss = beta * self.calculate_kl_loss(z_params)
+            activate_balancing = (beta < 1)
+            kl_loss = beta * self.calculate_kl_loss(z_params,activate_balancing)
             loss = tf.math.reduce_mean(recon_loss + kl_loss)
             total_loss = loss + spectral_loss + bn_loss
             # self.add_loss(loss+spectral_loss)
         gradients = tape.gradient(total_loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_weights))
+        self.steps += 1
         return {
             "loss": total_loss,
             "reconstruction_loss": recon_loss,
@@ -115,12 +120,15 @@ class NVAE(tf.keras.Model):
             "spectral_loss": spectral_loss,
         }
 
-    def calculate_kl_loss(self, z_params: List[DistributionParams]):
+    def calculate_kl_loss(self, z_params: List[DistributionParams], balancing):
         # z_params: enc_mu, enc_log_sigma, dec_mu, dec_log_sigma
         # -KL(q(z1|x)||p(z1)) - sum[ KL(q(zl|x,z<l) || p(z|z<l))]
         kl_per_group = []
         # n_groups x batch_size x 4
         loss = 0
+
+		
+
         for g in z_params:
             enc_sigma = tf.math.exp(g.enc_log_sigma)
             dec_sigma = tf.math.exp(g.dec_log_sigma)
@@ -133,7 +141,37 @@ class NVAE(tf.keras.Model):
             tf.convert_to_tensor(kl_per_group, dtype=tf.float32), axis=[0]
         )
 
-        return loss
+		if balancing:
+            kl_alphas = calculate_kl_alphas(self.encoder.n_latent_scales,self.encoder.n_groups_per_scale)
+			kl_coeffs = calculate_kl_coeff(0.0001)
+            kl_all = tf.stack(kl_per_group, 1)
+            kl_vals = tf.reduce_mean(kl_all,dim=0)
+            kl_coeff_i = tf_reduce_mean(tf.math.abs(kl_all),dim=0) + 0.01
+            total_kl = tf.reduce_sum(kl_coeff_i)
+            kl_coeff_i = kl_coeff_i / kl_alphas * total_kl
+            kl_coeff_i = kl_coeff_i / tf.reduce_mean(kl_coeff_i,1)
+            kl = tf.reduce_sum(kl_all*kl_coeff_i,1)
+            return kl
+        else:
+        	return loss
+	
+    # Calculates the balancer coefficients alphas. The coefficient decay for later groups,
+    # for which original paper offer several functions. Here, a square function is used.
+	def calculate_kl_alphas(self, num_scales, groups_per_scale):
+        coeffs = []
+        for i in range(num_scales):
+			coeffs.append(np.square(2**i)/groups_per_scale[num_scales-i-1] * tf.ones([groups_per_scale[num_scales-i-1]],tf.float32))
+		coeffs = tf.concat(coeffs, 0)
+        coeffs /= tf.reduce_min(coeffs)
+        return coeffs
+
+	def calculate_kl_coeff(min_kl_coeff):
+        #TODO: add as arg
+        kl_const_portion = 0.0001
+		warmup_iters = 0.3*self.n_total_iterations
+        const_iters = kl_const_portion*self.n_total_iterations
+        return max(min((self.steps - const_iters)/warmup_iters,1.0),min_kl_coeff)
+
 
     def on_epoch_begin(self, epoch, logs=None):
         self.epoch = epoch
