Fix missing gradients for encoder samples

models.py
@@ -121,10 +121,7 @@ class NVAE(tf.keras.Model):
             # warming up KL term for first 30% of training
             beta = min(self.epoch / (0.3 * self.total_epochs), 1)
             activate_balancing = beta < 1
-            if beta > 0:
-                kl_loss = beta * self.calculate_kl_loss(z_params, activate_balancing)
-            else:
-                kl_loss = 0
+            kl_loss = beta * self.calculate_kl_loss(z_params, activate_balancing)
             loss = tf.math.reduce_mean(recon_loss + kl_loss)
             total_loss = loss + bn_loss
         gradients = tape.gradient(total_loss, self.trainable_weights)